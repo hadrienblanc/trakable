f37fbd4ba043ee8036a2f0e1a2906980712d1312
Hadrien Blanc <blanc.hadrien@gmail.com>
2026-03-20 21:52:48 +0100

    fix: Fix CI failures on Ruby 3.1 and RuboCop 1.85
    - Require class_attribute explicitly in Controller (not auto-loaded
      on plain Ruby classes in some ActiveSupport/Ruby combos)
    - Disable RuboCop auto-plugin detection (plugins: []) to prevent
      rubocop-rake load error on CI

--- lib/trakable/controller.rb
+++ lib/trakable/controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'active_support/concern'
+require 'active_support/core_ext/class/attribute'
 
 module Trakable
   # Controller concern for automatically setting whodunnit from current_user.
